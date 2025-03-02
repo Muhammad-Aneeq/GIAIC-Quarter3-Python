3193c5fd5be475416fd408744fd19727f472e1b5
Muhammad-Aneeq <aneeqabdulsamad5761@gmail.com>
2025-03-02 23:43:01 +0500

    class 02 code

---
+++ class02/app.py
@@ -0,0 +1,13 @@
+# 1 upper
+name = "aneeq khatri"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(len(name))
+
+# 2 strips
+name = "  aneeq khatri  "
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
